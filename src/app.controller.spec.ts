faf1c7c2ab2223ae2cfd0268c4547c7c12bf2c76
Steven Oderayi <oderayi@gmail.com>
2021-12-12 23:58:54 +0100

    Add test

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,9 +1,121 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Board } from './lib/types';
 
 describe('AppController', () => {
   let appController: AppController;
+  const board: Board = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1],
+    [0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1],
+    [0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+    [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  ];
+  const largestRectangle = [
+    {
+      x: 5,
+      y: 4,
+    },
+    {
+      x: 5,
+      y: 5,
+    },
+    {
+      x: 5,
+      y: 6,
+    },
+    {
+      x: 5,
+      y: 7,
+    },
+    {
+      x: 5,
+      y: 8,
+    },
+    {
+      x: 6,
+      y: 4,
+    },
+    {
+      x: 6,
+      y: 5,
+    },
+    {
+      x: 6,
+      y: 6,
+    },
+    {
+      x: 6,
+      y: 7,
+    },
+    {
+      x: 6,
+      y: 8,
+    },
+    {
+      x: 7,
+      y: 4,
+    },
+    {
+      x: 7,
+      y: 5,
+    },
+    {
+      x: 7,
+      y: 6,
+    },
+    {
+      x: 7,
+      y: 7,
+    },
+    {
+      x: 7,
+      y: 8,
+    },
+    {
+      x: 7,
+      y: 9,
+    },
+    {
+      x: 8,
+      y: 4,
+    },
+    {
+      x: 8,
+      y: 5,
+    },
+    {
+      x: 8,
+      y: 6,
+    },
+    {
+      x: 8,
+      y: 7,
+    },
+    {
+      x: 8,
+      y: 8,
+    },
+    {
+      x: 8,
+      y: 9,
+    },
+    {
+      x: 9,
+      y: 7,
+    },
+  ];
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
@@ -14,9 +126,11 @@ describe('AppController', () => {
     appController = app.get<AppController>(AppController);
   });
 
-  describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+  describe('/largest-rectangle', () => {
+    it('should return coordinates of the largest rectangle', () => {
+      expect(appController.largestRectangle(board)).toStrictEqual(
+        largestRectangle,
+      );
     });
   });
 });
