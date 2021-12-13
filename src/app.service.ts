b51fd60907a1b9f0b18129870790e98d4bb1fc1b
Steven Oderayi <oderayi@gmail.com>
2021-12-13 14:44:00 +0100

    Update app service

--- src/app.service.ts
+++ src/app.service.ts
@@ -4,40 +4,29 @@ import { Board } from './lib/types';
 @Injectable()
 export class AppService {
   largestRectangle(board: Board): any {
-    const R = board.length; // Row length
-    const C = board[0].length; // Column length
-    const M = board; // Matrix
+    const R = board.length;
+    const C = board[0].length;
+    const M = board;
 
     const S = Array(R)
       .fill(null)
       .map(() => Array(C).fill(0));
 
-    // for (let y = 0; y < R; y++) {
-    //   S[y] = [];
-    //   for (let x = 0; x < C; x++) {
-    //     S[y][x] = 0;
-    //   }
-    // }
     let maxOfS: number, maxI: number, maxJ: number;
 
-    /* Copy first row of M to S*/
     S[0] = [...M[0]];
 
-    /* Copy first column of M to S*/
     let i: number, j: number;
     for (i = 0; i < R; i++) S[i][0] = M[i][0];
 
-    /* Construct other entries of S*/
     for (i = 1; i < R; i++) {
       for (j = 1; j < C; j++) {
-        if (M[i][j] == 1)
-          S[i][j] =
-            Math.min(S[i][j - 1], Math.min(S[i - 1][j], S[i - 1][j - 1])) + 1;
+        if (M[i][j] >= 1)
+          S[i][j] = Math.min(S[i][j - 1], Math.min(S[i - 1][j], S[i - 1][j - 1])) + 1;
         else S[i][j] = 0;
       }
     }
 
-    /* Find the maximum entry, and indexes of maximum entry in S */
     maxOfS = S[0][0];
     maxI = 0;
     maxJ = 0;
